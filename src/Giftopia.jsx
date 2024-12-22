b3ed462509580d1e148bb1a986b2ae91d5d44a93
Giovanni Coronel <elvis.coronel@unmsm.edu.pe>
2024-12-22 16:51:59 -0500

    add primeros carpetas y archivos

---
+++ src/Giftopia.jsx
@@ -0,0 +1,10 @@
+
+function Giftopia() {
+  return (
+    <>
+      <h1>Desde el componente principal</h1>
+    </>
+  )
+}
+
+export default Giftopia
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import Giftopia from './Giftopia'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Giftopia/>
   </StrictMode>,
 )
